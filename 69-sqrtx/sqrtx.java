cb8f2658518634479ff2390a141101e1d0de6857
harshisharma1812-rgb <harshi.sharma1812@gmail.com>
2026-06-27 19:40:49 +0530

    Time: 1 ms (97.76%) | Memory: 42.8 MB (22.76%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s = 1;
+        int e = x;
+        int ans = -1;
+
+        if(x == 0){
+            return 0;
+        }
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+
+            if(mid  == x/mid) {
+                return mid;
+        }
+        else if(mid > x/mid) {
+            //move to left 
+            e = mid - 1;
+        }
+        else {
+            //mid * mid < x
+            //potential solution par khade hai
+            //ans store karo and move to right, final ans k liye
+            ans = mid;
+            s = mid + 1; 
+        }
+        
+    }
+    return ans;
+  }
+
+}
