931cf8d7acc3b89922c0b4f5ccb1ac7624da360c
harshisharma1812-rgb <harshi.sharma1812@gmail.com>
2026-07-25 22:55:20 +0530

    Time: 1 ms (99.86%) | Memory: 46.6 MB (65.10%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    List<List<String>> ans = new ArrayList<>();
+
+    boolean[] col;
+    boolean[] diag1;
+    boolean[] diag2;
+
+    public List<List<String>> solveNQueens(int n) {
+
+        col = new boolean[n];
+        diag1 = new boolean[2 * n - 1];
+        diag2 = new boolean[2 * n - 1];
+
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        solve(0, board, n);
+
+        return ans;
+    }
+
+    private void solve(int row, char[][] board, int n) {
+
+        // Base Case
+        if (row == n) {
+
+            List<String> temp = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(board[i]));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+
+            int d1 = row - c + n - 1;
+            int d2 = row + c;
+
+            if (col[c] || diag1[d1] || diag2[d2])
+                continue;
+
+            // Place Queen
+            board[row][c] = 'Q';
+            col[c] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+
+            solve(row + 1, board, n);
+
+            // Backtrack
+            board[row][c] = '.';
+            col[c] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
